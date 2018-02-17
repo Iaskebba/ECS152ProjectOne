4b568e74070f62a9252d49ee518330281c6cca04
Ian Skebba <iaskebba@ucdavis.edu>
2018-02-17 15:12:48 -0800

    Basic Structure finished

--- project.py
+++ project.py
@@ -1,15 +1,7 @@
 import heapq, random, math, queue
 
-
-double negative-exponentially-distributed-time(double rate)
-{
-        double u;
-        u = drand48();
-        return ((-1/rate) * log(1-u));
-}
-
 # given lambda = .1 = rate
-def negative_exponential_distibution(rate):
+def Negative_Exponential_Distribution(rate):
     u = random.uniform(0,1)
     return ((-1/rate) * math.log(1-u))
 
@@ -21,28 +13,29 @@ class Event:
     def __cmp__(self, other):
         return cmp(self.time_stamp, other.time_stamp)
 
+def Process_A(event_object):
+    
+def Process_D(event_object):
+
 def main():
-    MAXBUFFER = math.inf # This is a variable to each test case
-    random.expovariate()# THIS WILL BE LAMBDA
+    # Initialize 
     number_of_trials = 10000
+    global time, MAXBUFFER, length, buffer_queue, GEL 
+    time = 0 # Current Time
     length = 0 # Number of packets in queue (BUFFER) including those being transmitted by server
-    time = 0 # Current time
-    queue = Queue()
-    GEL = []
+    MAXBUFFER = math.inf # This is a variable to each test case
+    buffer_queue = Queue() # buffer Queue
+    GEL = [] # Global Event List 
+
     first_event = Event('a',0)
     heapq.heappush(GEL, first_event)
 
-    # SET THE SERVICE RATE AND ARRIVAL RATE OF PACKETS
-    # Create first arrival event and insert into GEL.
-    # Event time of first arrival is obtained by adding random generated inter-arrival time to current time (0)
-    # u = 0 - 1 random
-    # lambda = 
     for i in range(number_of_trials):
-
-
-
-
-
+        event_object = heapq.heappop(GEL)
+        if event_object.event_type == 'a':
+            Process_A(event_object) 
+        else:
+            Process_D(event_object)
 
 if __name__ == "__main__":
     main()
