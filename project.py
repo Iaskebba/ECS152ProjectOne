d3c01d20a8e7eb37d06120e015ab5c1627b24f79
Ian Skebba <iaskebba@ucdavis.edu>
2018-03-08 22:11:20 -0800

    Starting phase 2

--- project.py
+++ project.py
@@ -22,8 +22,7 @@ def Process_A(event_object):
     global time, MAXBUFFER, buffer_queue, GEL, current_lambda, packet_drop, packet_length_array,used_server_time
     time = event_object.time_stamp
     next_arrival_event = Event('a',Negative_Exponential_Distribution(current_lambda) + time)
-    heapq.heappush(GEL, next_arrival_event)
-    
+    heapq.heappush(GEL, next_arrival_event) 
     packet_length_array.append(buffer_queue.qsize())
 
     # Test for space
@@ -41,8 +40,30 @@ def Process_A(event_object):
     else:
         packet_drop += 1
 
+
+def append_one_to_all_hosts(event_object):
+    host_one_buffer.append(Packet which will contain random byte size and random destination and time it got added event_object.time_stamp)
+    host_two_buffer.append(Packet which will contain byte size and destination)
+    ``
+    ``
+    ``
+    ``
+    ``
+    ``
+    ``
+    ``
+    ``
+    ``
+    ``
+
+def Process_Token(event_object):
+    depending on what node we are on
+    If node == 1:
+        check host_one_buffer if it has things then we need to send, else make new event for next time we need to check token
+
 def Process_D(event_object):
     global time, MAXBUFFER, buffer_queue, GEL, current_lambda, packet_drop, packet_length_array,used_server_time
+    append_one_to_all_hosts(event_object)
     time = event_object.time_stamp
     service_time = buffer_queue.get()
 
@@ -57,13 +78,12 @@ def Process_D(event_object):
 def main():
     # Initialize 
     number_of_trials = 100000
-    global time, MAXBUFFER, buffer_queue, GEL, current_lambda, packet_drop, packet_length_array,used_server_time
-    lambda_trials_infinite_buffer = [.1, .25, .4, .55, .65, .8, .9]
+    global time, MAXBUFFER, buffer_queue, GEL, current_lambda, packet_drop, packet_length_array,used_server_time,number_of_hosts
+    lambda_trials_infinite_buffer = [.01, .05, .1, .2, .3, .5, .6, .7, .8, .9]
     utilization_graph = []
     queue_length_graph = []
     for trial in lambda_trials_infinite_buffer:
         time = 0 # Current Time
-        #length = 0 # Number of packets in queue (BUFFER) including those being transmitted by server
         MAXBUFFER = math.inf # This is a variable to each test case
         buffer_queue = Queue() # buffer Queue
         GEL = [] # Global Event List 
@@ -82,8 +102,10 @@ def main():
             event_object = heapq.heappop(GEL)
             if event_object.event_type == 'a':
                 Process_A(event_object) 
-            else:
+            else if event = 'd':
                 Process_D(event_object)
+            else:
+                Process_Token(event_object)
 
         utilization_graph.append(used_server_time/time)
         queue_length_graph.append(sum(packet_length_array) / len(packet_length_array))
@@ -102,44 +124,7 @@ def main():
     plt.xlabel('Lambda Rate Value')
     plt.title('Average Queue Length for Infinite Buffer')
     plt.show()
-    buffer_sizes = [1,20,50]
-    lambda_trials = [.2,.4,.6,.8,.9]
-    for buffer_size in buffer_sizes:
-        packet_drop_graph = []
-        for trial in lambda_trials:
-            time = 0 # Current Time
-            #length = 0 # Number of packets in queue (BUFFER) including those being transmitted by server
-            MAXBUFFER = buffer_size # This is a variable to each test case
-            buffer_queue = Queue() # buffer Queue
-            GEL = [] # Global Event List 
-            
-            #statistics
-            packet_length_array = []
-            used_server_time = 0
-            packet_drop = 0
-            
-            current_lambda = trial
 
-            first_event = Event('a',0)
-            heapq.heappush(GEL, first_event)
-
-            for i in range(number_of_trials):
-                event_object = heapq.heappop(GEL)
-                if event_object.event_type == 'a':
-                    Process_A(event_object) 
-                else:
-                    Process_D(event_object)
-            packet_drop_graph.append(packet_drop)
-            print("lambda: " + str(trial) + " buffer: " + str(buffer_size))
-            print("Utilization: " + str(used_server_time / time))
-            print("Mean Queue Length: " + str(sum(packet_length_array) / len(packet_length_array)))
-            print("Packet Drop: " + str(packet_drop))
-            print()
-        plt.plot(lambda_trials,packet_drop_graph)
-        plt.ylabel('Packets Dropped')
-        plt.xlabel('Lambda Rate Value')
-        plt.title('Packets Dropped for Buffer Size ' + str(buffer_size))
-        plt.show()
 
 if __name__ == "__main__":
     main()
